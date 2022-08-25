56e66d654790ce4b7a0f95011fac5811c807a304
Adelle Pitsas <apitsas4@gmail.com>
2022-08-25 19:36:26 -0400

    complete the book tests

--- src/book.js
+++ src/book.js
@@ -10,15 +10,48 @@ function buildMainCharacter(name, age, pronouns) {
   };
 }
 
-function saveReview(reviews) {
-  return reviews.push[reviews]
+
+function saveReview(newReview, reviews) {
+  if(reviews.includes(newReview) === false) {
+    return reviews.push(newReview);
+  }
+}
+//the includes function determines whether an
+// array includes a vertain value among it entries,
+// returning true or false. The if statement says, "If
+// the array reviews does not contain the new review,
+// then push the new reivew into the array"
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length*20
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book =
+  {
+  title: bookTitle,
+  mainCharacter: bookCharacter,
+  pageCount: bookTitle.length*20,
+  genre: "fantasy"
+  }
+  return book
+}
+
+// Because we already have information for createTitle and
+// buildMainCharacter, we were just focused on writeBook. I followed
+// the assertion statements to make sure the object returned
+// what we assumed they would
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+  return book
 }
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
