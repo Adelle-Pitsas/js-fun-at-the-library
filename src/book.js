0c76b755afb1766a43c47c47786048688761a607
Adelle Pitsas <apitsas4@gmail.com>
2022-08-24 20:19:55 -0400

    complete 6 tests

--- src/book.js
+++ src/book.js
@@ -1,11 +1,23 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+}
+
+function saveReview(reviews) {
+  return reviews.push[reviews]
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
