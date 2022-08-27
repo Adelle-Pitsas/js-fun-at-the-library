eb8d3075447006fe72b3bec7ea966dca7ab0f4f5
Adelle Pitsas <apitsas4@gmail.com>
2022-08-27 13:19:18 -0400

    complete tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf1) {
+  if (shelf1.length < 3) {
+    return shelf1.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+      if(shelf[i].title === title) {
+        return shelf.splice(i, 1)
+      }
+    }
+  }
+
+
+function listTitles(shelf2) {
+  var list = [];
+  for (var i=0; i<shelf2.length; i++) {
+    list.push(shelf2[i].title)
+  }
+    return list.join(', ')
+}
+
+function searchShelf(shelf3, bookTitle) {
+  for (var i = 0; i < shelf3.length; i++)
+  {
+    if (shelf3[i].title === bookTitle) {
+      return true }
+  }
+    return false
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
