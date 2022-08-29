93abbdaa8b98190c1f772253cfab0e289d8a6872
Adelle Pitsas <apitsas4@gmail.com>
2022-08-28 20:16:12 -0400

    finalize completed tests

--- src/Library.js
+++ src/Library.js
@@ -35,29 +35,6 @@ function checkoutBook(library, book, genre) {
 
 
 
-// function checkoutBook(library, book, genre) {
-//   var listOfGenres = Object.keys(library.shelves);
-//   console.log(library.shelves.fiction.includes(book.title))
-//   for (var i = 0; i < listOfGenres.length; i++)
-//     {
-//         if (listOfGenres[i] === 'fiction' && genre === 'fiction' && library.shelves.fiction.includes(book) === true)
-//         { library.shelves.fiction.pop(book);
-//         console.log('after unshelving fiction: ', library.shelves.fiction)
-//         return `You have now checked out ${book} from the ${library.name}`}
-//
-//         if (listOfGenres[i] === 'nonFiction' && genre === 'nonFiction' && library.shelves.nonFiction.includes(book) === true)
-//         { library.shelves.nonFiction.pop(book)
-//         return `You have now checked out ${book} from the ${library.name}`}
-//
-//         if (listOfGenres[i] === 'fantasy' && genre === 'fantasy' && library.shelves.fantasy.includes(book) === true)
-//         { library.shelves.fantasy.pop(book)
-//         return `You have now checked out ${book} from the ${library.name}`}
-//
-//         else { return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
-//         }
-//     }
-// }
-
 
 module.exports = {
   createLibrary,
